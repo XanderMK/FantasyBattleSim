efe5d4bd2fc6b3f8076309809692f067263876be
XanderMK <xandermkleiber@gmail.com>
2025-11-15 00:16:20 -0500

    Added sample project

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import static com.raylib.Jaylib.WHITE;
+import static com.raylib.Raylib.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InitWindow(1280, 720, "Fantasy Battle Sim");
+        SetTargetFPS(240);
+        Camera2D camera = new Camera2D()
+                .zoom(5.0f);
+
+        Texture tex = LoadTexture("resources/pingas.png");
+
+        Vector2 position = new Vector2();
+        position.x(camera.zoom() / 2.0f).y(camera.zoom() / 2.0f);
+
+        while (!WindowShouldClose()) {
+            if (IsKeyDown(KEY_LEFT)) position.x(position.x() - 1);
+            if (IsKeyDown(KEY_RIGHT)) position.x(position.x() + 1);
+            if (IsKeyDown(KEY_UP)) position.y(position.y() - 1);
+            if (IsKeyDown(KEY_DOWN)) position.y(position.y() + 1);
+
+            ClearBackground(WHITE);
+            BeginMode2D(camera);
+
+            DrawTextureEx(tex, position, 0.0f, 0.1f, WHITE);
+
+            EndMode2D();
+            DrawFPS(20, 20);
+            EndDrawing();
+        }
+
+        UnloadTexture(tex);
+
+        CloseWindow();
+    }
+}
